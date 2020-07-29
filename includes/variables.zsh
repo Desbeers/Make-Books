bbfadd859230d181e4f38b471608e338ba3da54d
Desbeers <58348935+Desbeers@users.noreply.github.com>
2020-07-30 01:33:44 +0200

    Set pandoc epub-chapter-level option based on depth of book

--- includes/variables.zsh
+++ includes/variables.zsh
@@ -39,10 +39,20 @@ if [[ -f $metadata_file ]]; then
     : ${EXPORT_DIR=$export/$author/$title}
     # Export title:
     : ${EXPORT_TITLE=$title}
-    
+    # Breakup the HTML files in small portions, depending on level.
+    # If a book has parts, header is shifted once, if it has books too
+    # the header is shifted twice.
+    if grep -q '{.part}' *.md ; then
+        EPUB_CHAPTER_LEVEL=2
+    elif grep -q '{.book}' *.md ; then
+        EPUB_CHAPTER_LEVEL=3
+    else
+        EPUB_CHAPTER_LEVEL=1
+    fi
+    print "Level "$EPUB_CHAPTER_LEVEL
 fi
 
-export SOURCE_DIR BUILD_DIR BUILD_TITLE EXPORT_DIR EXPORT_TITLE
+export SOURCE_DIR BUILD_DIR BUILD_TITLE EXPORT_DIR EXPORT_TITLE EPUB_CHAPTER_LEVEL
 
 # Function to reset all 'dynamic' variables:
 clear_variables() {
