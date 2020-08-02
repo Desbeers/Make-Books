ef91244ac46103afbed9e4a986dcd47853707b33
Desbeers <58348935+Desbeers@users.noreply.github.com>
2020-08-02 20:40:27 +0200

    Removed all cruft

--- pandoc/filters/desbeers-chapterprecis.lua
+++ pandoc/filters/desbeers-chapterprecis.lua
@@ -2,53 +2,25 @@
     
     chapter précis Lua filter for Pandoc.
 
-   Only for ePub output. LaTeX sees it as 'subparagraph'.
+   Only for ePub output. LaTeX is smarter.
       
 --]]
 
 function Header(el)
-    content = pandoc.utils.stringify(el.content)
-    if el.level == 2 then
-        if FORMAT:match 'html' or FORMAT:match 'epub' then
+    if FORMAT:match 'epub' then
+        content = pandoc.utils.stringify(el.content)
+        if el.level == 2 then
             el = pandoc.RawBlock('html',string.format('<div class="section">%s</div>',content))
         end
-    end
-    if el.level == 3 then
-        if FORMAT:match 'html' or FORMAT:match 'epub' then
+        if el.level == 3 then
             el = pandoc.RawBlock('html',string.format('<div class="subsection">%s</div>',content))
         end
-    end
-    if el.level == 4 then
-        if FORMAT:match 'html' or FORMAT:match 'epub' then
+        if el.level == 4 then
             el = pandoc.RawBlock('html',string.format('<div class="subsubsection">%s</div>',content))
+        end
+        if el.level == 6 then
+            el = pandoc.RawBlock('html',string.format('<div class="chapterprecis">%s</div>',content))
         end
     end
     return el
-end
-
--- debug
-function dump(t,indent)
-    local names = {}
-    if not indent then indent = "" end
-    for n,g in pairs(t) do
-        table.insert(names,n)
-    end
-    table.sort(names)
-    for i,n in pairs(names) do
-        local v = t[n]
-        if type(v) == "table" then
-            if(v==t) then -- prevent endless loop if table contains reference to itself
-                print(indent..tostring(n)..": <-")
-            else
-                print(indent..tostring(n)..":")
-                dump(v,indent.."   ")
-            end
-        else
-            if type(v) == "function" then
-                print(indent..tostring(n).."()")
-            else
-                print(indent..tostring(n)..": "..tostring(v))
-            end
-        end
-    end
 end
