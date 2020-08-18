5e7e4f84b04721b6e0b17643dcee1c6bbf1f1ccc
Desbeers <58348935+Desbeers@users.noreply.github.com>
2020-08-18 17:09:55 +0200

    Fix for 'dedication' for ePubs

--- pandoc/filters/desbeers-linebreaks.lua
+++ pandoc/filters/desbeers-linebreaks.lua
@@ -7,7 +7,7 @@
 function HorizontalRule(el)
     if FORMAT:match 'latex' then
         el = pandoc.RawBlock('latex','\\bigskip')
-    elseif FORMAT:match 'epub' then
+    elseif FORMAT:match 'epub' or FORMAT:match 'html' then
         el = pandoc.RawBlock('html','<div class="bigskip" />')
     end
     return el
